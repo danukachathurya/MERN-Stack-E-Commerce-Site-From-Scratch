4ec5de02dff386cec4a4f9d67a9e1c38730a4cfc
Danuka Chathurya <danukachathurya8@gmail.com>
2024-01-19 02:02:04 +0530

    Add Rating Component

--- proshop-v2/frontend/src/components/Product.js
+++ proshop-v2/frontend/src/components/Product.js
@@ -1,27 +1,33 @@
-import React from 'react';
-import { Card } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import Rating from "./Rating";
 
 const product = ({ product }) => {
   return (
     <Card className="my-3 p-3 rounded">
-      <Link to={'/product/${product._id}'}>
+      <Link to={"/product/${product._id}"}>
         <Card.Img src={product.image} variant="top" />
       </Link>
 
       <Card.Body>
-        <Link to={'/product/${product._id}'}> 
-          <Card.Title as="div">
+        <Link to={"/product/${product._id}"}>
+          <Card.Title as="div" className='product-title'>
             <strong>{product.name}</strong>
           </Card.Title>
         </Link>
 
-        <Card.Text as="h3">
-          ${product.price}
+        <Card.Text as="div">
+          <Rating
+            value={product.rating}
+            text={`${product.numReviews} reviews`}
+          />
         </Card.Text>
+
+        <Card.Text as="h3">${product.price}</Card.Text>
       </Card.Body>
     </Card>
-  )
-}
+  );
+};
 
-export default product
+export default product;

41eb302242598468b569ef8a5b57dba2370dbae6
Danuka Chathurya <danukachathurya8@gmail.com>
2024-01-28 13:39:41 +0530

    Auth Middleware & Endpoint

--- proshop-v2/backend/middleware/errorMiddleware.js
+++ proshop-v2/backend/middleware/errorMiddleware.js
@@ -4,7 +4,7 @@ const notFound = (req, res, next) => {
   next(error);
 };
 
-const errorHandler = (error, req, res, next) => {
+const errorHandler = (err, req, res, next) => {
   let statusCode = res.statusCode === 200 ? 500 : res.statusCode;
   let message = err.message;
 

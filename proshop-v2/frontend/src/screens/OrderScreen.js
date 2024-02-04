8f59440f98a083264d1efa39d8ef25b37fd50fba
Danuka Chathurya <danukachathurya8@gmail.com>
2024-02-04 16:05:22 +0530

    PayPal Buttons

--- proshop-v2/frontend/src/screens/OrderScreen.js
+++ proshop-v2/frontend/src/screens/OrderScreen.js
@@ -11,11 +11,15 @@ import {
   ListGroupItem,
 } from "react-bootstrap";
 import { toast } from "react-toastify";
-import { useSelector } from 'react-redux';
-import { PayPalButtons, usePayPalScriptReducer } from '@paypal/react-paypal-js'
+import { useSelector } from "react-redux";
+import { PayPalButtons, usePayPalScriptReducer } from "@paypal/react-paypal-js";
 import Message from "../components/Message";
 import Loader from "../components/Loader";
-import { useGetOrderDetailsQuery, usePayOrderMutation, useGetPayPalClientIdQuery } from "../slices/ordersApiSlice";
+import {
+  useGetOrderDetailsQuery,
+  usePayOrderMutation,
+  useGetPayPalClientIdQuery,
+} from "../slices/ordersApiSlice";
 
 const OrderScreen = () => {
   const { id: orderId } = useParams();
@@ -27,26 +31,30 @@ const OrderScreen = () => {
     error,
   } = useGetOrderDetailsQuery(orderId);
 
-  const [payOrder, { isLoading:loadingPay }] = usePayOrderMutation();
+  const [payOrder, { isLoading: loadingPay }] = usePayOrderMutation();
 
   const [{ isPending }, paypalDispatch] = usePayPalScriptReducer();
 
-  const { data: paypal, isLoading: loadingPayPal, error: errorPayPal } = useGetPayPalClientIdQuery();
+  const {
+    data: paypal,
+    isLoading: loadingPayPal,
+    error: errorPayPal,
+  } = useGetPayPalClientIdQuery();
 
-  const { userInfo } = useSelector((state => state.auth));
+  const { userInfo } = useSelector((state) => state.auth);
 
   useEffect(() => {
-    if(!errorPayPal && !loadingPayPal && paypal.clientId) {
+    if (!errorPayPal && !loadingPayPal && paypal.clientId) {
       const loadPayPalScript = async () => {
         paypalDispatch({
-          type: 'resetOptions',
+          type: "resetOptions",
           value: {
-            'client-id': paypal.clientId,
-            currency: 'USD',
-          }
+            "client-id": paypal.clientId,
+            currency: "USD",
+          },
         });
-        paypalDispatch({ type: 'setLoadingStatus', value: 'pending' });
-      }
+        paypalDispatch({ type: "setLoadingStatus", value: "pending" });
+      };
       if (order && !order.isPaid) {
         if (!window.paypal) {
           loadPayPalScript();
@@ -54,6 +62,41 @@ const OrderScreen = () => {
       }
     }
   }, [order, paypal, paypalDispatch, loadingPayPal, errorPayPal]);
+
+  function onApprove(data, actions) {
+    return actions.order.capture().then(async function (details) {
+      try {
+        await payOrder({orderId, details});
+        refetch();
+        toast.success("Payment successful");
+      } catch (err) {
+        toast.error(err?.data?.message || err.message);
+      }
+    });
+  }
+  async function onApproveTest() {
+    await payOrder({ orderId, details: { payer: {} } });
+    refetch();
+    toast.success("Payment successful");
+  }
+  function onError(err) {
+    toast.error(err.message);
+  }
+  function createOrder(data, actions) {
+    return actions.order
+      .create({
+        purchase_units: [
+          {
+            amount: {
+              value: order.totalPrice,
+            },
+          },
+        ],
+      })
+      .then((orderId) => {
+        return orderId;
+      });
+  }
 
   return isLoading ? (
     <Loader />
@@ -108,14 +151,11 @@ const OrderScreen = () => {
                       <Image src={item.image} alt={item.name} fluid rounded />
                     </Col>
                     <Col>
-                    <Link to={`/products/${item.product}`}>
-                          {item.name}
-                    </Link>
+                      <Link to={`/products/${item.product}`}>{item.name}</Link>
                     </Col>
                     <Col md={4}>
-                      {item.qty} x ${item.price} = $
-                          {item.qty * item.price }
-                      </Col>
+                      {item.qty} x ${item.price} = ${item.qty * item.price}
+                    </Col>
                   </Row>
                 </ListGroup.Item>
               ))}
@@ -133,7 +173,7 @@ const OrderScreen = () => {
                 <Row>
                   <Col>Items</Col>
                   <Col>${order.itemsPrice}</Col>
-                </Row>             
+                </Row>
 
                 <Row>
                   <Col>Shipping</Col>
@@ -150,7 +190,32 @@ const OrderScreen = () => {
                   <Col>${order.totalPrice}</Col>
                 </Row>
               </ListGroup.Item>
-              {/* PAY ORDER PLACEHOLDER */}
+
+              {!order.isPaid && (
+                <ListGroup.Item>
+                  {loadingPay && <Loader />}
+
+                  {isPending ? (
+                    <Loader />
+                  ) : (
+                    <div>
+                      <Button
+                        onClick={onApproveTest}
+                        style={{ marginBottom: "10px" }}
+                      >
+                        Test Pay Order
+                      </Button>
+                      <div>
+                        <PayPalButtons
+                          createOrder={createOrder}
+                          onApprove={onApprove}
+                          onError={onError}
+                        ></PayPalButtons>
+                      </div>
+                    </div>
+                  )}
+                </ListGroup.Item>
+              )}
               {/* MARK AS DELIVERED PLACEHOLDER */}
             </ListGroup>
           </Card>

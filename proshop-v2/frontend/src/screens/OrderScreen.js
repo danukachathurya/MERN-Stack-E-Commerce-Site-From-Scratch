7556a69e14f2f4933abb212374ac36bcfa92e9ea
Danuka Chathurya <danukachathurya8@gmail.com>
2024-02-05 18:14:04 +0530

    List Orders For Admin &  Deliver Order Status

--- proshop-v2/frontend/src/screens/OrderScreen.js
+++ proshop-v2/frontend/src/screens/OrderScreen.js
@@ -19,6 +19,7 @@ import {
   useGetOrderDetailsQuery,
   usePayOrderMutation,
   useGetPayPalClientIdQuery,
+  useDeliverOrderMutation,
 } from "../slices/ordersApiSlice";
 
 const OrderScreen = () => {
@@ -32,6 +33,7 @@ const OrderScreen = () => {
   } = useGetOrderDetailsQuery(orderId);
 
   const [payOrder, { isLoading: loadingPay }] = usePayOrderMutation();
+  const [deliverOrder, { isLoading: loadingDeliver }] = useDeliverOrderMutation();
 
   const [{ isPending }, paypalDispatch] = usePayPalScriptReducer();
 
@@ -96,6 +98,16 @@ const OrderScreen = () => {
       .then((orderId) => {
         return orderId;
       });
+  }
+
+  const deliverOrderHandler = async () => {
+    try {
+      await deliverOrder({orderId});
+      refetch();
+      toast.success("Order Delivered");
+    } catch (err) {
+      toast.error(err?.data?.message || err.message);
+    }
   }
 
   return isLoading ? (
@@ -216,7 +228,20 @@ const OrderScreen = () => {
                   )}
                 </ListGroup.Item>
               )}
-              {/* MARK AS DELIVERED PLACEHOLDER */}
+
+              { loadingDeliver && <Loader /> }
+
+              { userInfo && userInfo.isAdmin && order.isPaid && !order.isDelivered && (
+                <ListGroup.Item>
+                  <Button
+                    type='button'
+                    className='btn btn-block'
+                    onClick={deliverOrderHandler}
+                  >
+                    Mark As Delivered    
+                  </Button>
+                </ListGroup.Item>
+              )}
             </ListGroup>
           </Card>
         </Col>

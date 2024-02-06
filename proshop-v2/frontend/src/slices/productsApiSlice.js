1350817ec3d8100ac14db5bc03a2ce601204f902
Danuka Chathurya <danukachathurya8@gmail.com>
2024-02-06 20:53:13 +0530

    Upload Product Image

--- proshop-v2/frontend/src/slices/productsApiSlice.js
+++ proshop-v2/frontend/src/slices/productsApiSlice.js
@@ -1,4 +1,4 @@
-import { PRODUCTS_URL } from "../constants";
+import { PRODUCTS_URL, UPLOAD_URL } from "../constants";
 import { apiSlice } from "./apiSlice";
 
 export const productsApiSlice = apiSlice.injectEndpoints({
@@ -30,6 +30,14 @@ export const productsApiSlice = apiSlice.injectEndpoints({
         body: data,
       }),
       invalidatesTags: ['Product'],
+    }),
+    uploadProductImage: builder.mutation({
+      query: (data) => ({
+        url: `${UPLOAD_URL}`, 
+        method: 'POST',
+        body: data,
+      }),
+      
     })
   }),
 });
@@ -39,4 +47,5 @@ export const {
   useGetProductDetailsQuery,
   useCreateProductMutation,
   useUpdateProductMutation,
+  useUploadProductImageMutation
 } = productsApiSlice;

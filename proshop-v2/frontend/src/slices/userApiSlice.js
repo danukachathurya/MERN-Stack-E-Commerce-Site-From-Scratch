bb7381ccb5b8d03b9b76de2611fe0385e9fe1905
Danuka Chathurya <danukachathurya8@gmail.com>
2024-02-07 08:09:19 +0530

    Update Users

--- proshop-v2/frontend/src/slices/userApiSlice.js
+++ proshop-v2/frontend/src/slices/userApiSlice.js
@@ -63,6 +63,20 @@ export const userApiSlice = apiSlice.injectEndpoints({
         method: 'DELETE',
       }),
     }),
+    getUserDetails: builder.query({
+      query: (userId) => ({
+        url: `${USERS_URL}/${userId}`,
+      }),
+      keepUnusedDataFor: 5,
+    }),
+    updateUser: builder.mutation({
+      query: (data) => ({
+        url: `${USERS_URL}/${data.userId}`,
+        method: 'PUT',
+        body: data,
+      }),
+      invalidatesTags: ['Users'],
+    })
   }),
 });
 

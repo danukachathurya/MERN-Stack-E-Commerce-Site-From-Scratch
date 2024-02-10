29989e7b2b1def5d855107028344abe656c8b830
Danuka Chathurya <danukachathurya8@gmail.com>
2024-02-10 13:03:25 +0530

    Prepare For Production

--- proshop-v2/backend/server.js
+++ proshop-v2/backend/server.js
@@ -10,7 +10,7 @@ import connectDB from './config/db.js';
 import uploadRoutes from './routes/uploadRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
 
 connectDB(); // connect to MongoDB
 
@@ -22,10 +22,6 @@ app.use(express.urlencoded({ extended: true }));
 
 // Cookie parser middleware
 app.use(cookieParser());
-
-app.get('/', (req, res) => {
-  res.send('API is running...');
-});
 
 app.use('/api/products', productRoutes); 
 app.use('/api/users', userRoutes); 
@@ -39,7 +35,23 @@ app.get('/api/config/paypal', (req, res) =>
 const __dirname = path.resolve(); //Set __dirname to current directory
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
+if (process.env.NODE_ENV === 'production') {
+  // set static folder
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+  // any route that is not api will be redirected to index.html
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running...');
+  });
+}
+
 app.use(notFound);
 app.use(errorHandler);
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(port, () => 
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`)
+);
